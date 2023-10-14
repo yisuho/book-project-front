ba0ae2f16b7938e0df8952a2378f9028d66203c7
yisuho <yeek0620@gmail.com>
2023-10-14 16:33:04 +0900

    baseUrl 수정

--- frontend/src/api/axiosInstance.tsx
+++ frontend/src/api/axiosInstance.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "ec2-35-78-241-42.ap-northeast-1.compute.amazonaws.com",
   headers: { Authorization: `Bearer ${sessionStorage.getItem("userToken")}` },
 });
