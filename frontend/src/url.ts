d6f71bd0705d5f3de3eb5640f2d2e3fc6433c53c
yisuho <yeek0620@gmail.com>
2023-10-17 15:48:47 +0900

    url 변경

--- frontend/src/url.ts
+++ frontend/src/url.ts
@@ -1,1 +1,3 @@
-export const url = "https://www.bookbear.shop";
+// export const url = "https://www.bookbear.shop";
+export const url =
+  "https://ec2-35-78-241-42.ap-northeast-1.compute.amazonaws.com";
